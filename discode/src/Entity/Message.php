79343fd6e41a914660c392bd8b64baa9ee6f6daa
allan <bro.allan@laposte.net>
2020-03-31 09:25:42 +0200

    <EVO> creation entité room et message
    fix de .env

---
+++ discode/src/Entity/Message.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_creation;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $epingler;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Room", mappedBy="messages")
+     */
+    private $room_id;
+
+    public function __construct()
+    {
+        $this->room_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $date_creation): self
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getEpingler(): ?bool
+    {
+        return $this->epingler;
+    }
+
+    public function setEpingler(bool $epingler): self
+    {
+        $this->epingler = $epingler;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Room[]
+     */
+    public function getRoomId(): Collection
+    {
+        return $this->room_id;
+    }
+
+    public function addRoomId(Room $roomId): self
+    {
+        if (!$this->room_id->contains($roomId)) {
+            $this->room_id[] = $roomId;
+            $roomId->setMessages($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRoomId(Room $roomId): self
+    {
+        if ($this->room_id->contains($roomId)) {
+            $this->room_id->removeElement($roomId);
+            // set the owning side to null (unless already changed)
+            if ($roomId->getMessages() === $this) {
+                $roomId->setMessages(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ discode/src/Entity/Room.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RoomRepository")
+ */
+class Room
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Message", inversedBy="room_id")
+     */
+    private $messages;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMessages(): ?Message
+    {
+        return $this->messages;
+    }
+
+    public function setMessages(?Message $messages): self
+    {
+        $this->messages = $messages;
+
+        return $this;
+    }
+}
