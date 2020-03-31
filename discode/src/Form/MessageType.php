3a22b913644b034cc3a52a2a0fe54f862d71b77f
allan <bro.allan@laposte.net>
2020-03-31 11:08:21 +0200

    <EVO> add Room and Message CRUD

---
+++ discode/src/Form/MessageType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_creation')
+            ->add('message')
+            ->add('epingler')
+            ->add('room')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
