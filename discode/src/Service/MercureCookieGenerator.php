52b19c0595a3a3e8141818765d9a47a37eb09d6a
allan <bro.allan@laposte.net>
2020-04-03 10:48:18 +0200

    test

--- discode/src/Service/MercureCookieGenerator.php
+++ discode/src/Service/MercureCookieGenerator.php
@@ -6,6 +6,7 @@ namespace App\Service;
 
 use App\Entity\Room;
 use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Signer\Hmac\Sha384;
 
 class MercureCookieGenerator
@@ -24,8 +25,8 @@ class MercureCookieGenerator
     public function generate(Room $room){
 
         $token = (new Builder())
-            ->set('mercure', ['send'=>["http://192.168.1.22/room/{$room->getId()}"]])
-            ->sign(new Sha384(),$this->secret)
+            ->set('mercure', ['send'=>["http://192.168.1.22/room/".$room->getId()]])
+            ->sign(new Sha256(),$this->secret)
             ->getToken();
         return "mercureAuthorization={$token}; Path=/hub; HttpOnly;";
     }
